eacc3fcd4dad2bc5087ed27333877521c0472a2e
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2024-01-31 13:23:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 3748059
-api_hash = "f8c9df448f3ba20a900bc2ffc8dae9d5"
-bot_token = "6934333025:AAEMtr5HKH6Vty8HDVkp31BEjJvh-Amwum4"
-auth_users = "5591734243, 1993514215, 5735651588, 870101201"
+api_id = 3498045
+api_hash = "229441a8b355de58ba025be657c82ea3"
+bot_token = "6685547719:AAHRce4N90dhZu-Kx2cJm_KO_wvV_Sdtzco"
+auth_users = "6478217960"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "5735651588"
+osowner_users = "6478217960"
 owner_users = [int(num) for num in osowner_users.split(",")]
